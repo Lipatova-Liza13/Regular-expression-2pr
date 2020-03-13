ba8a7761d52eac6b0f036ecf29de76e1887ae57b
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-03-13 23:12:02 +0200

    2PR-version 0.1

---
+++ CheckTELnumb/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CheckTELnumb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string End = "ON", zn;
+            do
+            {
+                
+                Console.WriteLine("Введите, пожалуйста, свою строку ");
+                string line = Console.ReadLine();
+                Regex reg = new Regex(@"(\(\+27\)|\d)\d{2}-\d{3}-\d{4}");
+                if ( reg.IsMatch(line))
+                {
+                    MatchCollection matches1 = reg.Matches(line);
+                    Console.WriteLine("В телефонной книге было найдено ");
+                    Console.WriteLine(matches1.Count + "  Номеров формата ххх-ххх-хххх и (+24)хх-ххх-хххх");
+                    foreach (Match match in matches1)
+                        Console.WriteLine(match.Value);
+                }
+                else
+                {
+                    Console.WriteLine("К сожалению, но в вашей строке не было найдено ни одного совпадения");
+                    Console.WriteLine("Попробуйте ещё раз)");
+                }
+                Console.WriteLine("Понравилось, хотите продолжить?");
+                Console.WriteLine("Для завершения программы задайте ей номер  - название (из маленьких букв)");
+                zn = Console.ReadLine();
+                Regex refinal = new Regex(@"^\d+-[a-z]+$");
+                if (refinal.IsMatch(zn)) End = "STOP";
+
+            } while (End != "STOP");
+            Console.WriteLine("Спасибо, хороший выбор");
+        }
+    }
+}
---
+++ ListOFlists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ListOFlists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string End="ON", zn;
+            do
+            {
+                Console.WriteLine("Введите, пожалуйста, свой список списков: ");
+                string line = Console.ReadLine();
+                Regex reg = new Regex("^[a-z](,[a-z])*(;[a-z](,[a-z])*)*$");
+                if (reg.IsMatch(line))
+                {
+                    Regex r = new Regex("[;]*[a-z](,[a-z])*[;]*");
+                    MatchCollection matches = r.Matches(line);
+                    Console.WriteLine("В вашем списке " + matches.Count + " списка");
+                    foreach (Match match in matches)
+                        Console.WriteLine(Regex.Replace(match.Value,
+                            ";",//что меняю
+                            ""));//на что меняю
+                }
+                else
+                {
+                    Console.WriteLine("К сожалению, но вы ввели список списков не в соответствующем формате");
+                    Console.WriteLine("Ошибка может быть в содержании цифр или других символов (кроме букв , комы и точки с запятой) в вашом списке ");
+                    Console.WriteLine("Также ошибку может вызвать полное отсутствие символов");
+                }
+                Console.WriteLine("Понравилось, хотите продолжить?");
+                Console.WriteLine("Для завершения программы задайте ей номер  - название (из букв)");
+                zn= Console.ReadLine();
+                Regex refinal = new Regex(@"^\d+-[a-z]+$");
+                if (refinal.IsMatch(zn)) End = "STOP";
+
+            } while (End != "STOP");
+            Console.WriteLine("Спасибо, хороший выбор");
+        }
+    }
+}
